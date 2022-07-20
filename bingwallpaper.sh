f83ca3a2859737e9a85af56089acb6b058e71536
SMSourov <81343486+SMSourov@users.noreply.github.com>
2022-07-20 17:16:09 +0600

    Startup script.

---
+++ bingwallpaper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script runs the task scheduler file.
+# This script should run in the first place.
+
+# Go to the location of the task scheduler 
+# file.
+cd /mnt/sda6/BingWallpaperLinux-main
+
+# Run the task scheduler file.
+powershell ./bingwallpaper.ps1
